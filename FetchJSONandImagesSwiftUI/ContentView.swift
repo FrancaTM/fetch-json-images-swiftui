cfe19d83282035728684d70d02caad1c90e2e5aa
FrancaTM <francatm@outlook.com>
2019-06-12 22:50:11 -0300

    sample finished

--- FetchJSONandImagesSwiftUI/ContentView.swift
+++ FetchJSONandImagesSwiftUI/ContentView.swift
@@ -42,10 +42,58 @@ struct ContentView: View {
 
     var body: some View {
         NavigationView {
-            List(networkManager.courses.identified(by: \.name)) {
-                Text($0.name)
+            List(networkManager.courses.identified(by: \.name)) { course in
+                CourseRowView(course: course)
             }.navigationBarTitle(Text("Courses"))
         }
+    }
+}
+
+struct CourseRowView: View {
+    let course: Course
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            ImageView(imageUrl: course.imageUrl)
+            Text(course.name)
+        }
+    }
+}
+
+class ImageLoader: BindableObject {
+    var didChange = PassthroughSubject<Data, Never>()
+
+    var data = Data() {
+        didSet {
+            didChange.send(data)
+        }
+    }
+
+    init(imageUrl: String) {
+        // fetch image then call didChange
+        guard let url = URL(string: imageUrl) else { return }
+
+        URLSession.shared.dataTask(with: url) { data, _, _ in
+            guard let data = data else { return }
+
+            DispatchQueue.main.async {
+                self.data = data
+            }
+        }.resume()
+    }
+}
+
+struct ImageView: View {
+    @ObjectBinding var imageLoader: ImageLoader
+
+    init(imageUrl: String) {
+        imageLoader = ImageLoader(imageUrl: imageUrl)
+    }
+
+    var body: some View {
+        Image(uiImage: (imageLoader.data.count == 0) ? UIImage(named: "apple")! : UIImage(data: imageLoader.data)!)
+            .resizable()
+            .frame(width: 320, height: 180)
     }
 }
 
